7d6a2c12c906c9271ff3c5beecdf8a024f4da981
小山由人 <koyama@reytech.co.jp>
2022-04-16 16:41:31 +0900

    404のハンドリングを追加

---
+++ src/main/java/com/raisetech/apprunnersample/CustomExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.raisetech.apprunnersample;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.HashMap;
+
+@EnableWebMvc
+@RestControllerAdvice
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public HashMap<String, String> handleNoHandlerFound(NoHandlerFoundException e, WebRequest request) {
+        HashMap<String, String> response = new HashMap<>();
+        response.put("status", "fail");
+        response.put("message", "resource was not found");
+        response.put("detail_message", e.getLocalizedMessage());
+        return response;
+    }
+}
